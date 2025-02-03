c4ae971445b9fce233ab0ee442a242998891e41a
Gustavo Domingos <gustavo.domingos@hotmail.com>
2025-02-03 13:54:50 -0300

    projeto inicial

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert(' Boas Vindas ao jogo : NÚMERO SECRETO');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um numero entre 1 a 10:');
+
+if (chute == numeroSecreto){
+    console.log('Isso ai!! Você acertou o número secreto');
+}
